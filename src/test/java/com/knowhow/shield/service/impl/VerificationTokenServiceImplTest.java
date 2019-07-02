693a6380b5b10c24ba7adfd4a852a69bdc9ca8f4
Ahmed Abou Raya <a.abouraya@gmail.com>
2019-07-02 18:36:03 +0200

    increase the unit test for VerificationTokenService

--- src/test/java/com/knowhow/shield/service/impl/VerificationTokenServiceImplTest.java
+++ src/test/java/com/knowhow/shield/service/impl/VerificationTokenServiceImplTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.knowhow.shield.Exception.VerificationTokeIsNotValidException;
 import com.knowhow.shield.model.User;
 import com.knowhow.shield.model.VerificationToken;
 import com.knowhow.shield.repository.VerificationTokenRepository;
@@ -15,8 +16,11 @@ import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Optional;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
@@ -35,12 +39,22 @@ public class VerificationTokenServiceImplTest {
 
     private VerificationTokenService verificationTokenService;
 
+    private User user;
+
+    private VerificationToken verificationToken;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
     private final static LocalDate LOCAL_DATE = LocalDate.of(2000, 01, 01);
 
     @Before
     public void init() {
         verificationTokenService = new VerificationTokenServiceImpl(clock, verificationTokenRepository);
         fixedClock = Clock.fixed(LOCAL_DATE.atStartOfDay(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
+
+        user = mock(User.class);
+        verificationToken = mock(VerificationToken.class);
         doReturn(fixedClock.instant()).when(clock).instant();
         doReturn(fixedClock.getZone()).when(clock).getZone();
     }
@@ -48,7 +62,6 @@ public class VerificationTokenServiceImplTest {
     @Test
     public void givenUserExpectCreateVerificationTokenValidFor24H() {
         //Arrange
-        User user = mock(User.class);
         VerificationToken verificationToken = mock(VerificationToken.class);
         when(verificationTokenRepository.save(any(VerificationToken.class))).thenReturn(verificationToken);
         ArgumentCaptor<VerificationToken> tokenCaptor = ArgumentCaptor.forClass(VerificationToken.class);
@@ -61,10 +74,9 @@ public class VerificationTokenServiceImplTest {
     }
 
     @Test
-    public void givenSameUserExpectDifferntToken() {
+    public void givenSameUserExpectDifferentToken() {
         //Arrange
         User user = mock(User.class);
-        VerificationToken verificationToken = mock(VerificationToken.class);
         when(verificationTokenRepository.save(any(VerificationToken.class))).thenReturn(verificationToken);
 
         //Act
@@ -73,5 +85,74 @@ public class VerificationTokenServiceImplTest {
 
         //Assert
         assertThat(token1).isNotEqualTo(token2);
+    }
+
+    @Test
+    public void givenValidTokenToFindUserExpectValidUser() {
+        //Arrange
+        when(verificationToken.getExpiryDate()).thenReturn(LocalDateTime.now(fixedClock).plusHours(1));
+        when(verificationToken.getUser()).thenReturn(user);
+        when(verificationTokenRepository.findByTokenAndActiveTrue("1234")).thenReturn(Optional.of(verificationToken));
+
+        //Act
+        User result = verificationTokenService.findUserByToken("1234");
+
+        //Assert
+        assertThat(result).isEqualTo(user);
+    }
+
+    @Test
+    public void givenInvalidTokenToFindUserExpectException() {
+        //Arrange
+        when(verificationTokenRepository.findByTokenAndActiveTrue("1234")).thenReturn(Optional.empty());
+        exception.expect(VerificationTokeIsNotValidException.class);
+        exception.expectMessage("this token: 1234 is invalid");
+
+        //Act
+        verificationTokenService.findUserByToken("1234");
+
+        //Assert is done by Rule
+    }
+
+    @Test
+    public void givenExpiredTokenToFindUserExpectException() {
+        //Arrange
+        when(verificationTokenRepository.findByTokenAndActiveTrue("1234")).thenReturn(Optional.of(verificationToken));
+        when(verificationToken.getExpiryDate()).thenReturn(LocalDateTime.now(fixedClock).minusHours(25));
+        exception.expect(VerificationTokeIsNotValidException.class);
+        exception.expectMessage("this token: 1234 is expired");
+
+        //Act
+        verificationTokenService.findUserByToken("1234");
+
+        //Assert is done by Rule
+    }
+
+    @Test
+    public void givenValidTokenToDeactivateExpectDeactivated() {
+        //Arrange
+        when(verificationTokenRepository.findByTokenAndActiveTrue("1234")).thenReturn(Optional.of(verificationToken));
+        when(verificationTokenRepository.save(any(VerificationToken.class))).thenReturn(verificationToken);
+        ArgumentCaptor<VerificationToken> tokenCaptor = ArgumentCaptor.forClass(VerificationToken.class);
+
+        //Act
+        verificationTokenService.deactivateToken("1234");
+
+        //Assert
+        verify(verificationTokenRepository).save(tokenCaptor.capture());
+        assertThat(tokenCaptor.getValue().isActive()).isFalse();
+    }
+
+    @Test
+    public void givenInvalidTokenToDeactivateExpectException() {
+        //Arrange
+        when(verificationTokenRepository.findByTokenAndActiveTrue("1234")).thenReturn(Optional.empty());
+        exception.expect(VerificationTokeIsNotValidException.class);
+        exception.expectMessage("this token: 1234 is invalid");
+
+        //Act
+        verificationTokenService.deactivateToken("1234");
+
+        //Assert is done by Rule
     }
 }
