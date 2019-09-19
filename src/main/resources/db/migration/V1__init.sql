5cca4efa9d7a3fd579f7b28a860bb593851ccdb9
Ahmed Abou Raya <a.abouraya@gmail.com>
2019-09-19 15:04:11 +0200

    create OAUTH schema

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -30,6 +30,13 @@ create table users_roles
     primary key (user_id, role_id)
 );
 
+-- CREATE TABLE IF NOT EXISTS authorities
+-- (
+--     username  VARCHAR(256) NOT NULL,
+--     authority VARCHAR(256) NOT NULL,
+--     PRIMARY KEY (username, authority)
+-- );
+
 create table verification_token
 (
     id          bigint       not null,
@@ -40,9 +47,62 @@ create table verification_token
     primary key (id)
 );
 
+CREATE TABLE IF NOT EXISTS oauth_client_details
+(
+    client_id               VARCHAR(256) PRIMARY KEY,
+    resource_ids            VARCHAR(256),
+    client_secret           VARCHAR(256) NOT NULL,
+    scope                   VARCHAR(256),
+    authorized_grant_types  VARCHAR(256),
+    web_server_redirect_uri VARCHAR(256),
+    authorities             VARCHAR(256),
+    access_token_validity   INTEGER,
+    refresh_token_validity  INTEGER,
+    additional_information  VARCHAR(4000),
+    autoapprove             VARCHAR(256)
+);
+
+CREATE TABLE IF NOT EXISTS oauth_client_token
+(
+    token_id          VARCHAR(256),
+    token             BLOB,
+    authentication_id VARCHAR(256) PRIMARY KEY,
+    user_name         VARCHAR(256),
+    client_id         VARCHAR(256)
+);
+
+CREATE TABLE IF NOT EXISTS oauth_access_token
+(
+    token_id          VARCHAR(256),
+    token             BLOB,
+    authentication_id VARCHAR(256),
+    user_name         VARCHAR(256),
+    client_id         VARCHAR(256),
+    authentication    BLOB,
+    refresh_token     VARCHAR(256)
+);
+
+CREATE TABLE IF NOT EXISTS oauth_refresh_token
+(
+    token_id       VARCHAR(256),
+    token          BLOB,
+    authentication BLOB
+);
+
+CREATE TABLE IF NOT EXISTS oauth_code
+(
+    code           VARCHAR(256),
+    authentication BLOB
+);
+
 INSERT INTO users (email, enabled, first_name, last_name, password, token_expired, id)
 VALUES ('admin@shild.com', true, 'admin', 'ad', '$2a$10$rq6wC6tqbqoZtx3r9aLx1Op8dXZlS88hNxIX/JBx0.YevQkTI4zae',
         false, '21ae7719e3f64465ba7ed66cfec9fa90');
+
+INSERT INTO oauth_client_details (client_id, client_secret, scope, authorized_grant_types, authorities,
+                                  access_token_validity)
+VALUES ('clientId', '{bcrypt}$2a$10$vCXMWCn7fDZWOcLnIEhmK.74dvK1Eh8ae2WrWlhr2ETPLoxQctN4.', 'read,write',
+        'password,refresh_token,client_credentials', 'ROLE_CLIENT', 300);
 
 -- alter table roles_privileges
 --     add constraint FK5duhoc7rwt8h06avv41o41cfy FOREIGN key (privilege_id) references privileges;
