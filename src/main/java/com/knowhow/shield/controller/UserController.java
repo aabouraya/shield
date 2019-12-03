03d4dcb91f45d1926879b379b4fe2abe21ad97bd
Ahmed Abou Raya <a.abouraya@gmail.com>
2019-12-03 12:45:32 +0200

    add context-path

--- src/main/java/com/knowhow/shield/controller/UserController.java
+++ src/main/java/com/knowhow/shield/controller/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/shield", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 class UserController {
 
     private UserService userService;
