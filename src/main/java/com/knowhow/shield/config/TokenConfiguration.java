ecc5b442087f97065162324f4f595737631d46ef
Ahmed Abou Raya <a.abouraya@gmail.com>
2020-02-04 16:45:45 +0200

    add additional information to the token

--- src/main/java/com/knowhow/shield/config/TokenConfiguration.java
+++ src/main/java/com/knowhow/shield/config/TokenConfiguration.java
@@ -1,16 +1,25 @@
 package com.knowhow.shield.config;
 
 import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Files;
+import com.knowhow.shield.dto.EnhancedUserDetails;
 import java.io.IOException;
 import java.security.KeyPair;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -30,6 +39,7 @@ public class TokenConfiguration {
                 .getKeyPair(jwt.getKeyPairAlias(), jwt.getKeyPairPassword().toCharArray());
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
         jwtAccessTokenConverter.setKeyPair(keyPair);
+        // jwtAccessTokenConverter.enhance(accessToken, authentication);
         jwtAccessTokenConverter.setVerifierKey(
                 Files.asCharSource(appProperties.getJwt().getPublicKey().getFile(), Charsets.UTF_8).read());
         return jwtAccessTokenConverter;
@@ -42,17 +52,43 @@ public class TokenConfiguration {
         return new JwtTokenStore(jwtAccessTokenConverter);
     }
 
+//    @Bean
+//    public JwtAccessTokenConverter accessTokenConverter() {
+//        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter(){
+//            @Override
+//            public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+//                if(authentication.getOAuth2Request().getGrantType().equalsIgnoreCase("password")) {
+//                    final Map<String, Object> additionalInfo = new HashMap<String, Object>();
+//                    authentication.getPrincipal();
+//
+//                    additionalInfo.put("organization", authentication.getName());
+//                    ((DefaultOAuth2AccessToken) accessToken)
+//                            .setAdditionalInformation(additionalInfo);
+//                }
+//                accessToken = super.enhance(accessToken, authentication);
+//                ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(new HashMap<>());
+//                return accessToken;
+//            }
+//        };
+//        // converter.setSigningKey("123");
+//        final KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("mytest.jks"), "mypass".toCharArray());
+//        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("mytest"));
+//        return converter;
+//    }
+
     @Bean
     @DependsOn("tokenStore")
     public DefaultTokenServices tokenServices(final TokenStore tokenStore,
             final ClientDetailsService clientDetailsService) {
         DefaultTokenServices tokenServices = new DefaultTokenServices();
         tokenServices.setSupportRefreshToken(true);
+        tokenServices.setTokenEnhancer((accessToken, auth) -> {
+            ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(ImmutableMap.of("party_id", "fixed_value"));
+            return accessToken;
+        });
         tokenServices.setTokenStore(tokenStore);
         tokenServices.setClientDetailsService(clientDetailsService);
         tokenServices.setAuthenticationManager(this.authenticationManager);
         return tokenServices;
     }
-
-
 }
---
+++ src/main/java/com/knowhow/shield/dto/EnhancedUserDetails.java
@@ -0,0 +1,24 @@
+package com.knowhow.shield.dto;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class EnhancedUserDetails extends org.springframework.security.core.userdetails.User {
+
+    private Optional<UUID> customerId;
+
+    public EnhancedUserDetails(String username, String password, boolean enabled, boolean accountNonExpired,
+            boolean credentialsNonExpired, boolean accountNonLocked,
+            Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+    }
+}
